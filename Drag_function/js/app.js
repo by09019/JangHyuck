524345c7a0004a867bd548578ea186e45a02ac7c
by09019 <skg09203@naver.com>
2019-10-01 19:31:29 +0900

    수정중

--- Drag_function/js/app.js
+++ Drag_function/js/app.js
@@ -7,19 +7,19 @@ eleTop = v => $(v).offset().top;
 let
 centerX = 0,
 centerY = 0,
-target = { me:null, x:0, y:0, center:{x:0, y:0},test:0 },
+target = { me:null, x:0, y:0, center:{x:0, y:0},test:0,left:0,top:0 },
 list = { arr:[], left:eleLeft(".list"),heightArr:[] },
 box = { left:eleLeft(".box_list"), top:eleTop(".box_list") },
 check = { first:0, out:0, isDown:false },
 other = {windowScroll:0,nowChk:0,ntcx:0,ntcy:0,moveIdx:0};
 
-function OriginalPlace() {
+function OriginalPlace() {// 밖에서 mouseup 시 원래 자리로
 	$(`.list:not(.list:nth-child(${target.me.index() + 1}))`).css({position:"relative",top:0,left:0,transform:"translate(0px,0px)",zIndex:"0",transition:"none"});
 	target.me.css({transition:"0.1s ease-out"}).css({transform:"translate(0px,0px)"});
 	setTimeout(function() { target.me.css({transition:"none",position:"relative",left:0,top:0}); },100.1);
 }
 
-function certainPart(e = 0,txt = "in") {
+function certainPart(e = 0,txt = "in") {//태그 위치 변경
 	if(txt == "out"){
 		$(`.list:not(.list:nth-child(${target.me.index()+1}))`).css({transform:`translate(0px,0px`});
 	}else if(txt == "in") {
@@ -30,14 +30,15 @@ function certainPart(e = 0,txt = "in") {
 				}
 			});
 			check.out = 0;
+		}else{
+			Array.from($(".list")).filter((v,idx) => {
+				if(idx > e && target.me.index() != idx){
+					$(v).css({transform:`translate(0px,${target.me.height() + 28}px)`});
+				}else if(target.me.index() != idx && idx <= e){
+					$(v).css({transform:`translate(0px,0px`});
+				}
+			});
 		}
-		Array.from($(".list")).filter((v,idx) => {
-			if(idx > e && target.me.index() != idx){
-				$(v).css({transform:`translate(0px,${target.me.height() + 28}px)`});
-			}else if(target.me.index() != idx && idx <= e){
-				$(v).css({transform:`translate(0px,0px`});
-			}
-		});
 	}
 }
 
@@ -53,6 +54,8 @@ function mouseDown(e) {//마우스 다운시
 	if(target.me == null) {
 		check.isDown = true;
 		target.me = $(this);
+		target.me.left = target.me.offset().left;
+		target.me.top = target.me.offset().top;
 		target.x = eleLeft(target.me);
 		target.y = eleTop(target.me);
 		target.me.css({position:"fixed",zIndex:"9999"});
@@ -72,8 +75,8 @@ function mouseMove(e) {
 	if(check.isDown){//마우스 클릭시에만
 		target.center.x = e.pageX - (target.me.width() / 2);
 		target.center.y = e.pageY - (target.me.height() / 2);
-		centerX = (target.center.x + (target.me.width() / 2) );
-		centerY = (target.center.y + (target.me.height() / 2) );
+		centerX = (eleLeft(target.me) + (target.me.width() / 2) );
+		centerY = (eleTop(target.me) + (target.me.height() / 2) );
 		target.me.css({transform:`translate(${(target.center.x - target.x) - $(window).scrollLeft()}px,
 			${target.center.y - target.y - $(window).scrollTop()}px)`});
 
@@ -89,14 +92,25 @@ function mouseMove(e) {
 
 		}else {// false = 안
 			Array.from($(".list")).forEach((v,idx) => {
-				ntcx = eleLeft(v);
-				ntcy = eleTop(v);
-				if(centerX > ntcx && centerX < (ntcx + $(v).width()) &&
-					centerY > ntcy && centerY < (ntcy + $(v).height() + 28) ){
-					if(other.nowChk != idx && idx != target.me.index()){
-						other.nowChk = certainPart(idx);
+				// if(centerX > list.left && centerX < (list.left + $(v).width()) &&
+				// 	centerY > list.arr[idx].top && centerY < (list.arr[idx].top + list.arr[idx].height + 28) ){
+				// 	if(other.moveIdx != idx){
+				// 		other.nowChk = certainPart(idx);
+				// 		setTimeout(function(){
+				// 			list.arr[idx].height = $(v).height();
+				// 			console.log(list.arr[idx].height);
+				// 		},150.1);
+				// 		other.moveIdx = idx;
+				// 		// console.log(idx);
+				// 		return certainPart(idx);
+				// 	}
+				// }
+
+				if(centerX > list.left && centerX < (list.left + $(v).width()) &&
+					eleTop(target.me) >= (eleTop(v) + ($(v).height() / 2)) ||
+					(eleTop(target.me) + target.me.height()) >= (eleTop(v) + ($(v).height() / 2))  ){
+					if(other.moveIdx != idx){
 						other.moveIdx = idx;
-						console.log(idx);
 						return certainPart(idx);
 					}
 				}
@@ -117,29 +131,24 @@ function mouseUp(e) {//마우스 업 했을때
 			if(other.moveIdx == target.me.index()){
 				OriginalPlace();
 			}else{
+				target.me.insertAfter(`.list:nth-child(${other.moveIdx + 1})`);
 				setTimeout(function() {
 					target.me.css({transition:"0.1s ease-out"}).css({top:0,transform:`translate(0px,${(eleTop(`.list:nth-child(${other.moveIdx +1})`) + $(`.list:nth-child(${other.moveIdx +1})`).height() + 28 - other.windowScroll )}px)`});
-					// setTimeout(function() {
-					// 	Array.from($(".list")).forEach((v,idx) => {
-
-					// 	});
-					// },100.1);
 				},150.1);
 			}
-
 		}
-
 		check.isDown = false;
 		setTimeout(function() {
-			$(".box_list").css({background:"#ebecf0"});
 			check.first = 0;
 			target.me = null;
-		},152.11);
-
+			$(".box_list").css({background:"#ebecf0"});
+			$(".list").css({transition:"none",position:"relative",left:0,top:0,transform:"translate(0px,0px)",zIndex:0}).removeClass("hov");
+		},150.21);
 	}
 }
 
 function eve() {
+	setTimeout( _ => $(window).scrollTop(0) ,0.01);
 	$(window).on("resize",function() {
 		box.left = eleLeft(".box_list");
 		list.left = eleLeft(".list");
@@ -152,14 +161,10 @@ function eve() {
 }
 
 function load() {
-	setTimeout( _ => $(window).scrollTop(0) ,0.01);
-	Array.from($(".list")).forEach((v,idx) => list.arr.push({top:eleTop(v),idx:idx,height:($(v).height() + 20)}) );
-	list.arr.forEach((v,idx) => {
-		$(".testBorder").eq(idx).css({top:v.top + v.height});
-	});
+	Array.from($(".list")).forEach((v,idx) => {
+		list.arr.push({top:eleTop(v),idx:idx,height:($(v).height() + 20)});
+		$(v).find(".test").css({width:"100%",height:"1px",position:"absolute",top:($(v).height() / 2),background:"black",left:0 });
+	} );
 }
-$(document).ready(function() {
-	
-});
 eve();
 load();
